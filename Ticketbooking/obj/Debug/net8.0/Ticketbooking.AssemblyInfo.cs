522afa3a192814dc0e0f71f0f1acbe56b4fea24e
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2025-02-23 11:15:30 +0700

    test solution path

--- Ticketbooking/obj/Debug/net8.0/Ticketbooking.AssemblyInfo.cs
+++ Ticketbooking/obj/Debug/net8.0/Ticketbooking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ticketbooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28da0fcd5dcbe861b72126a11bcd389cb23ed2c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9013ad5d82d4788b371c1a8836b02fa64630ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ticketbooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ticketbooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
